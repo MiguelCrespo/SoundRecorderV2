57d06a6fd5fba43b6e9974fc1b2871c67c78a470
Miguel Crespo <miguel.crespo6@gmail.com>
2018-09-30 00:57:22 -0500

    First commit

---
+++ app/src/androidTest/java/co/happydevelopers/soundrecorderv2/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package co.happydevelopers.soundrecorderv2
+
+import androidx.test.InstrumentationRegistry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+//@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("co.happydevelopers.soundrecorderv2", appContext.packageName)
+    }
+}
